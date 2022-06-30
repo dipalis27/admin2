98b5b9b1be9a0535745e949091a2711c810b0eeb
sonus.ror@gmail.com <Sonu Singh>
2022-06-30 13:45:25 +0530

    Made changes for brand settings

--- template-app/app/serializers/bx_block_admin/banner_serializer.rb
+++ template-app/app/serializers/bx_block_admin/banner_serializer.rb
@@ -2,6 +2,13 @@ module BxBlockAdmin
 
   class BannerSerializer < BuilderBase::BaseSerializer
     attributes :id, :banner_position, :web_banner
+    attribute :attachments do |object|
+      if object.attachments.present?
+        BxBlockAdmin::AttachmentSerializer.new(object.attachments)
+      else
+        []
+      end
+    end
   end
   
 end
