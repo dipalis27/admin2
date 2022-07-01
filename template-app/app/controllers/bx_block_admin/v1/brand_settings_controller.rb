9140766b6adb256754f5eb21646c89d22fa5c3b6
sonus.ror@gmail.com <Sonu Singh>
2022-07-01 15:28:58 +0530

    Made changes for brand settings

--- template-app/app/controllers/bx_block_admin/v1/brand_settings_controller.rb
+++ template-app/app/controllers/bx_block_admin/v1/brand_settings_controller.rb
@@ -21,8 +21,10 @@ module BxBlockAdmin
         end
         @brand_setting = BxBlockStoreProfile::BrandSetting.new(brand_settings_params)
         @brand_setting.logo.attach(data: params[:logo]) if params[:logo].present?
+        @brand_setting.favicon_logo.attach(data: params[:favicon_logo]) if params[:favicon_logo].present?
         if @brand_setting.valid?
-          response = BxBlockBanner::Banner.validate_and_save(params[:banners])
+          response = BxBlockBanner::Banner.validate_and_save(params[:banners]) if params[:banners].present?
+
           if response[:success]
             @brand_setting.save
             return render json: BrandSettingSerializer.new(@brand_setting), status: :ok
@@ -39,7 +41,8 @@ module BxBlockAdmin
         if @brand_setting
           if @brand_setting.update(brand_settings_params)
             @brand_setting.logo.attach(data: params[:logo]) if params[:logo].present?
-            response = BxBlockBanner::Banner.validate_and_save(params[:banners])
+            @brand_setting.favicon_logo.attach(data: params[:favicon_logo]) if params[:favicon_logo].present?
+            response = BxBlockBanner::Banner.validate_and_save(params[:banners]) if params[:banners].present?
             return render json: BrandSettingSerializer.new(@brand_setting), status: :ok
           end
         else
@@ -88,7 +91,7 @@ module BxBlockAdmin
       private
 
       def brand_settings_params
-        params.permit(:heading, :sub_heading, :header_color, :common_button_color, :button_hover_color, :brand_text_color, :active_tab_color, :inactive_tab_color, :active_text_color, :inactive_text_color, :country, :currency_type, :phone_number, :fb_link, :instagram_link, :twitter_link, :youtube_link, :button_hover_text_color, :border_color, :sidebar_bg_color, :copyright_message, :wishlist_icon_color, :wishlist_btn_text_color, :order_detail_btn_color, :api_key, :auth_domain, :database_url, :project_id, :storage_bucket, :messaging_sender_id, :app_id, :measurement_id, :is_facebook_login, :is_google_login, :is_apple_login, :transparent_color, :grey_color, :black_color, :white_color, :primary_color, :background_grey_color, :extra_button_color, :header_text_color, :header_subtext_color, :background_color, :secondary_color, :secondary_button_color, :address, :gst_number, :highlight_primary_color, :highlight_secondary_color, :address_state_id, :template_selection, :color_palet, :whatsapp_number, :whatsapp_message)
+        params.permit(:heading, :sub_heading, :header_color, :common_button_color, :button_hover_color, :brand_text_color, :active_tab_color, :inactive_tab_color, :active_text_color, :inactive_text_color, :country, :currency_type, :phone_number, :fb_link, :instagram_link, :twitter_link, :youtube_link, :button_hover_text_color, :border_color, :sidebar_bg_color, :copyright_message, :wishlist_icon_color, :wishlist_btn_text_color, :order_detail_btn_color, :api_key, :auth_domain, :database_url, :project_id, :storage_bucket, :messaging_sender_id, :app_id, :measurement_id, :is_facebook_login, :is_google_login, :is_apple_login, :transparent_color, :grey_color, :black_color, :white_color, :primary_color, :background_grey_color, :extra_button_color, :header_text_color, :header_subtext_color, :background_color, :secondary_color, :secondary_button_color, :address, :gst_number, :highlight_primary_color, :highlight_secondary_color, :address_state_id, :template_selection, :color_palet, :whatsapp_number, :whatsapp_message, :navigation_item1, :navigation_item2, :is_whatsapp_integration)
       end
 
       def banner_params
