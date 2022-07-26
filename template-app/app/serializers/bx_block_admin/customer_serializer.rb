188dd3d7b7c39b9620c934506bb7352946bcf91c
Ishwar <ishwar.d@protonshub.com>
2022-07-26 15:17:29 +0530

    Image url in serializer changes

--- template-app/app/serializers/bx_block_admin/customer_serializer.rb
+++ template-app/app/serializers/bx_block_admin/customer_serializer.rb
@@ -1,9 +1,13 @@
 module BxBlockAdmin
   class CustomerSerializer < BuilderBase::BaseSerializer
-    attributes :id, :full_name, :email, :activated, :full_phone_number, :image
+    attributes :id, :full_name, :email, :activated, :full_phone_number
 
     attribute :delivery_addresses do |object|
       DeliveryAddressSerializer.new(object.delivery_addresses).serializable_hash
+    end
+
+    attribute :image do |object|
+      $hostname + Rails.application.routes.url_helpers.rails_blob_url(object.image, only_path: true) if object.image.attached?
     end
   end
 end
