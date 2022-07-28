6c4d91e2337d1354c5980c819748f3c1dfed423d
sonus.ror@gmail.com <Sonu Singh>
2022-07-28 16:39:32 +0530

    Made changes for country and currency in brand settings

--- template-app/app/serializers/bx_block_admin/store_detail_serializer.rb
+++ template-app/app/serializers/bx_block_admin/store_detail_serializer.rb
@@ -1,5 +1,12 @@
 module BxBlockAdmin
   class StoreDetailSerializer < BuilderBase::BaseSerializer
-    attributes :id, :heading, :currency_type, :phone_number, :country, :address_state_id, :address, :zipcode
+    attributes :id, :heading, :currency_type, :phone_number, :address_state_id, :address, :zipcode
+    attribute :country do |object|
+      if object.store_country.present?
+        object.store_country.name
+      else
+        object.country
+      end
+    end
   end
 end
