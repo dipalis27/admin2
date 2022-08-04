a549fa899380a38a4cd40b126d3f781bb975abf3
proton <somendra.s@protonshub.com>
2022-08-04 15:32:48 +0530

    Revert "status issue resolved"
    This reverts commit d62ecf3ec8248af05b28acc8d43a1aeab52383bf.

--- template-app/app/serializers/bx_block_admin/payment_serializer.rb
+++ template-app/app/serializers/bx_block_admin/payment_serializer.rb
@@ -1,9 +1,13 @@
 module BxBlockAdmin
   class PaymentSerializer < BuilderBase::BaseSerializer
-    attributes :id, :configuration_type, :api_key, :api_secret_key, :razorpay_account_id
+    attributes :id, :configuration_type, :api_key, :api_secret_key, :razorpay_account_id, :razorpay_variables
 
     attribute :razorpay_account_id do
       ENV['RAZORPAY_ACCOUNT_ID']
+    end 
+
+    attribute :razorpay_variables do
+      (ENV['RAZORPAY_KEY'] && ENV['RAZORPAY_SECRET']).present?
     end 
   end
 end
