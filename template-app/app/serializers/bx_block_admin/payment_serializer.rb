f6ccfafb2e93a61ae26ad1a228498055dc0971e8
proton <somendra.s@protonshub.com>
2022-08-04 15:30:31 +0530

    razorpay response added

--- template-app/app/serializers/bx_block_admin/payment_serializer.rb
+++ template-app/app/serializers/bx_block_admin/payment_serializer.rb
@@ -1,5 +1,9 @@
 module BxBlockAdmin
   class PaymentSerializer < BuilderBase::BaseSerializer
-    attributes :id, :configuration_type, :api_key, :api_secret_key
+    attributes :id, :configuration_type, :api_key, :api_secret_key, :razorpay_account_id
+
+    attribute :razorpay_account_id do
+      ENV['RAZORPAY_ACCOUNT_ID']
+    end 
   end
 end
