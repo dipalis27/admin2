5e2d9735ac31f64c8a7cceaffe7b1d7fde3eca4f
sonus.ror@gmail.com <Sonu Singh>
2022-08-12 15:31:04 +0530

    Implemented charts data

--- template-app/app/controllers/bx_block_admin/v1/order_reports_controller.rb
+++ template-app/app/controllers/bx_block_admin/v1/order_reports_controller.rb
@@ -10,6 +10,15 @@ module BxBlockAdmin
           ]}, status: :unprocessable_entity
         end
       end
+
+      def get_sales_chart_data
+        response = BxBlockAdmin::OrderReport.new.get_sales(params)
+        if response
+          render json: {data: response}, status: :ok
+        else
+          render json: {errors: ["Order Report Not Found"]}, status: :unprocessable_entity
+        end
+      end
     end
   end
 end
