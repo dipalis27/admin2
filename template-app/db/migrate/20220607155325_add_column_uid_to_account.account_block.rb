94ff948f2ca8a1b4345292b0b7575b6779ac6bd9
Ishwar <ishwar.d@protonshub.com>
2022-08-25 18:33:29 +0530

    New admin panel migrations

--- template-app/db/migrate/20220607155325_add_column_uid_to_account.account_block.rb
+++
@@ -1,6 +0,0 @@
-# This migration comes from account_block (originally 20210122024210)
-class AddColumnUidToAccount < ActiveRecord::Migration[6.0]
-  def change
-    add_column :accounts, :uuid, :string
-  end
-end
