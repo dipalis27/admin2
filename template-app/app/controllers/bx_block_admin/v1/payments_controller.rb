a5e17c9c3985c37d2fbd5aa0442411f03f071366
proton <somendra.s@protonshub.com>
2022-09-16 17:02:25 +0530

    fix: changes in the index api and removed the get_status api

--- template-app/app/controllers/bx_block_admin/v1/payments_controller.rb
+++ template-app/app/controllers/bx_block_admin/v1/payments_controller.rb
@@ -7,12 +7,17 @@ module BxBlockAdmin
 
       def index
         brand =  BxBlockStoreProfile::BrandSetting.last
-        api = brand.country == "india"? BxBlockApiConfiguration::ApiConfiguration.find_by(configuration_type: "razorpay") : BxBlockApiConfiguration::ApiConfiguration.find_by(configuration_type: "stripe")
 
-        if api.present?
-          render json: PaymentSerializer.new(api).serializable_hash, success: :ok
+        if brand.country == "india"
+          if razorpay_default_credentials_available?
+            render json: razorpay_variable_response, status: :ok
+          else
+            api = BxBlockApiConfiguration::ApiConfiguration.where(configuration_type:'razorpay')
+            render json: PaymentSerializer.new(api).serializable_hash, success: :ok
+          end
         else
-          render json:{error:"No API configurations found"}, status: 200
+          api = BxBlockApiConfiguration::ApiConfiguration.find_by(configuration_type: "stripe")
+          render json: PaymentSerializer.new(api).serializable_hash, success: :ok
         end
       end
 
@@ -38,25 +43,6 @@ module BxBlockAdmin
         end
       end
 
-      def get_status
-        if (ENV['RAZORPAY_KEY'] && ENV['RAZORPAY_SECRET']).present?
-          render json: {
-            data:{
-              attributes:{
-                api_key: ENV['RAZORPAY_KEY'],
-                user_name: ENV['USER_NAME'],
-                api_secret_key: '-',
-                razorpay_account_id: ENV['RAZORPAY_ACCOUNT_ID'],
-                razorpay_variables: (ENV['RAZORPAY_KEY'] && ENV['RAZORPAY_SECRET']).present?
-              }
-            }
-          }
-        else
-          razorpay = BxBlockApiConfiguration::ApiConfiguration.find_by(configuration_type: "razorpay")
-          render json: PaymentSerializer.new(razorpay).serializable_hash, status: :ok
-        end
-      end
-
       private
 
       def api_params
@@ -69,6 +55,23 @@ module BxBlockAdmin
         rescue 
           render json: {"errors": "API configuration not found"}, status: 404
         end
+      end
+
+      def razorpay_default_credentials_available?
+        ENV['RAZORPAY_KEY'].present? && ENV['RAZORPAY_SECRET'].present?
+      end
+
+      def razorpay_variable_response
+        {data:{
+            attributes:{
+              api_key: ENV['RAZORPAY_KEY'],
+              user_name: ENV['USER_NAME'],
+              api_secret_key: '-',
+              razorpay_account_id: ENV['RAZORPAY_ACCOUNT_ID'],
+              razorpay_variables: razorpay_default_credentials_available?
+            }
+          }
+        }
       end
     end
   end
--- template-app/app/serializers/bx_block_admin/payment_serializer.rb
+++ template-app/app/serializers/bx_block_admin/payment_serializer.rb
@@ -1,13 +1,5 @@
 module BxBlockAdmin
   class PaymentSerializer < BuilderBase::BaseSerializer
-    attributes :id, :configuration_type, :api_key, :api_secret_key, :razorpay_account_id, :razorpay_variables
-
-    attribute :razorpay_account_id do
-      ENV['RAZORPAY_ACCOUNT_ID']
-    end 
-
-    attribute :razorpay_variables do
-      (ENV['RAZORPAY_KEY'] && ENV['RAZORPAY_SECRET']).present?
-    end 
+    attributes :id, :configuration_type, :api_key, :api_secret_key
   end
 end
