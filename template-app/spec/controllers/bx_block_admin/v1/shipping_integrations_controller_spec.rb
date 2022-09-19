1191ddd8065f25ea77ffb4d10a274ecec8190153
proton <somendra.s@protonshub.com>
2022-09-19 22:45:54 +0530

    docs:specs updated for payment anf shipping_integrations

--- template-app/spec/controllers/bx_block_admin/v1/shipping_integrations_controller_spec.rb
+++ template-app/spec/controllers/bx_block_admin/v1/shipping_integrations_controller_spec.rb
@@ -7,15 +7,35 @@ RSpec.describe BxBlockAdmin::V1::ShippingIntegrationsController, type: :controll
     @admin_user = FactoryBot.create(:admin_user, email: 'admin4@example.com', role: 'super_admin')
     @token = BuilderJsonWebToken::AdminJsonWebToken.encode(@admin_user.id)
     @request_params = { token: @token, format: :json }
+    @shiprocket_variables_present = ENV['SHIPROCKET_USER_EMAIL'].present? && ENV['SHIPROCKET_USER_PASSWORD'].present?
+    @shiprocket_variables_not_present = !(ENV['SHIPROCKET_USER_EMAIL'].present? && ENV['SHIPROCKET_USER_PASSWORD'].present?)
     @success_response_code = 200
   end
 
   describe 'api configurations' do
     context 'index' do
-      it 'returns api configurations if success' do
-        FactoryBot.create(:api_configuration, configuration_type: 'shiprocket', ship_rocket_user_email: 'admin@example.com', ship_rocket_user_password: 'password')
-        get :index, params: @request_params.merge(configuration_type: 'shiprocket')
-        expect(JSON.parse(response.body)['data'].size).to eq(1)
+      it 'If country is India and shiprocket variables are present' do
+        @brand = FactoryBot.create(:brand_settings)
+        @brand.country == "india" && @shiprocket_variables_present
+        get :index, params: @request_params
+        expectation = HashWithIndifferentAccess.new({"ship_rocket_user_email" => ENV['SHIPROCKET_USER_EMAIL'], "ship_rocket_user_password"=>  ENV['SHIPROCKET_USER_PASSWORD'],  "shiprocket_variables"=> @shiprocket_variables_present})
+        response_attributes = JSON.parse(response.body)['data']['attributes']
+        expect(response_attributes).to eq(expectation)
+        expect(response.status).to eq(200)
+      end
+      
+      it 'If country is india and shiprocket variables are not present' do
+        @brand = FactoryBot.create(:brand_settings)
+        @brand.country == "india" && !@shiprocket_variables_not_present
+        get :index, params: @request_params.merge(configuration_type: 'shprocket')
+        expect(response.status).to eq(200)
+      end
+
+      it 'If country is uk' do
+        @brand = FactoryBot.create(:brand_settings, country: "uk")
+        @brand.country == "uk"
+        get :index, params: @request_params.merge(configuration_type: '525k')
+        expect(response.status).to eq(200)
       end
     end
 
--- template-app/spec/factories/bx_block_api_configuration/api_configuration.rb
+++ template-app/spec/factories/bx_block_api_configuration/api_configuration.rb
@@ -1,9 +1,7 @@
 FactoryBot.define do
   factory :api_configuration, class: 'BxBlockApiConfiguration::ApiConfiguration' do
     configuration_type{'razorpay'}
-    api_key{Faker::Lorem.characters(number: 10)}
-    api_secret_key{Faker::Lorem.characters(number: 10)}
-    ship_rocket_user_email {'admin@example.com'}
-    ship_rocket_user_password {'password'}
+    api_key{"n/a"}
+    api_secret_key{"n/a"}
   end
 end
